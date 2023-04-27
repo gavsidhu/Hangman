bf348388a6186a9572afa11d5603873bde8840df
David <davidhariantowu@gmail.com>
2023-04-27 02:11:12 -0400

    Fixed a GET/POST bug

--- src/api/saveScore.js
+++ src/api/saveScore.js
@@ -1,6 +1,6 @@
 export const sendScore = ({ name, elapsedTime }) => {
     return fetch(`${process.env.REACT_APP_API_URL}/save_score`, {
-      method: "GET",
+      method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
